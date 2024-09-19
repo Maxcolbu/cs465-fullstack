3f3eb0fde8ecce58a2d614d8ad7940b726df962c
Maxim Colburn <maxim.colburn@snhu.edu>
2024-09-19 17:39:44 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,9 @@
-const travel = (reg, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+/* GET travel view */
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
